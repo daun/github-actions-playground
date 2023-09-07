2718faaa40635b6586037aa29ca3172971508926
Philipp Daun <post@philippdaun.net>
2023-09-07 13:15:27 +0200

    Update to use report action

--- .github/actions/playwright-report-comment/index.js
+++ .github/actions/playwright-report-comment/index.js
@@ -42,25 +42,25 @@ async function run(octokit, context, token) {
 	const cwd = process.cwd()
 
 	const reportFile = getInput('report-file')
-	const commenTitle = getInput('comment-title')
+	const commentTitle = getInput('comment-title')
 	const iconStyle = getInput('icon-style')
 
-	const { eventName } = context
-	const { owner, repo, number: pull_number } = context.issue
+	const { eventName, repo, payload } = context
+	const { owner, number: pull_number } = context.issue
 
 	try {
-		debug('pull request ' + JSON.stringify(context.payload, null, 2))
+		debug('pull request ' + JSON.stringify(payload, null, 2))
 	} catch (e) {}
 
 	let baseRef
 	let baseSha
 	if (eventName == 'push') {
-		baseRef = context.payload.ref
-		baseSha = context.payload.before
+		baseRef = payload.ref
+		baseSha = payload.before
 		console.log(`Commit pushed onto ${baseRef} (${baseSha})`)
 	} else if (eventName == 'pull_request' || eventName == 'pull_request_target') {
-		baseRef = context.payload.pull_request.base.ref
-		baseSha = context.payload.pull_request.base.sha
+		baseRef = payload.pull_request.base.ref
+		baseSha = payload.pull_request.base.sha
 		console.log(`PR #${pull_number} targeting ${baseRef} (${baseSha})`)
 	} else {
 		throw new Error(`Unsupported event type: ${eventName}. Only "pull_request", "pull_request_target", and "push" triggered workflows are currently supported.`)
@@ -75,8 +75,56 @@ async function run(octokit, context, token) {
 
 	const data = JSON.parse(await readFile(reportPath))
 	const report = parseReport(data)
+	const summary = renderReportSummary(report, { title: commentTitle, iconStyle })
 
+	const prefix = '<!-- playwright-report-github-action -->'
+	const body = `${prefix}\n\n${summary}`
 	let commentId = null
+
+	if (eventName !== 'pull_request' && eventName !== 'pull_request_target') {
+		console.log('No PR associated with this action run. Not posting a check or comment.')
+	} else {
+		startGroup(`Commenting test report on PR`)
+		try {
+			const { data: comments } = await octokit.issues.listComments({ ...repo, issue_number: pull_number })
+			const existingComment = comments.findLast(c => c.user.type === 'Bot' && c.body.includes(prefix)) || {}
+			commentId = existingComment.id || null
+		} catch (error) {
+			console.log(`Error fetching existing comments: ${error.message}`)
+		}
+
+		if (commentId) {
+			console.log(`Updating previous comment #${commentId}`)
+			try {
+				await octokit.issues.updateComment({ ...repo, comment_id: commentId, body })
+			} catch (error) {
+				console.log(`Error updating previous comment: ${error.message}`)
+				commentId = null
+			}
+		} else {
+			console.log('Creating new comment')
+			try {
+				const { data: newComment } = await octokit.issues.createComment({ ...repo, issue_number: pull_number, body })
+				commentId = newComment.id
+			} catch (error) {
+				console.log(`Error creating comment: ${error.message}`)
+				console.log(`Submitting PR review comment instead...`)
+				try {
+					const { issue } = context
+					await octokit.pulls.createReview({ owner, repo: issue.repo, pull_number: issue.number, event: 'COMMENT', body })
+				} catch (e) {
+					console.log(`Error creating PR review: ${error.message}`)
+				}
+			}
+		}
+		endGroup();
+	}
+
+	if (!commentId) {
+		const intro = `Unable to comment on your PR — this can happen for PR's originating from a fork without write permissions. You can copy the test results directly into a comment using the markdown summary below:`
+		console.log(`${intro}\n\n${body}`)
+	}
+
 	setOutput('comment-id', commentId)
 }
 
@@ -108,11 +156,15 @@ function parseReport(report) {
 	}
 }
 
-function renderTitle() {
-	return `### ${title}`
+function renderReportSummary(report, { title, iconStyle }) {
+	const paragraphs = []
+
+	paragraphs.push(`### ${title}`)
+
+	return paragraphs.map(p => p.trim()).filter(Boolean).join('\n\n')
 }
 
-function renderIcon(status) {
+function renderIcon(status, { iconStyle }) {
 	if (iconStyle === 'octicons') {
 		return createOcticonUrl(icons.octicons[status], { label: status, color: colors[status] })
 	} else {
